1f884d5ed0e90667f539b58b9075c4433f9e3b0b
Milind Daftari <milinddaftari@gmail.com>
2023-02-22 17:21:08 -0500

    CSRF compatibility commit

--- LegacySite/views.py
+++ LegacySite/views.py
@@ -17,6 +17,7 @@ def index(request):
     context= {'user': request.user}
     return render(request, "index.html", context)
 
+@csrf_protect
 # Register for the service.
 def register_view(request):
     if request.method == 'GET':
@@ -37,7 +38,7 @@ def register_view(request):
         u.save()
         return redirect("index.html")
         
-
+@csrf_protect
 # Log into the service.
 def login_view(request):
     if request.method == "GET":
@@ -57,12 +58,14 @@ def login_view(request):
             return render(request, "login.html", context)
         return redirect("index.html")
 
+@csrf_protect
 # Log out of the service.
 def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
     return redirect("index.html")
 
+@csrf_protect
 def buy_card_view(request, prod_num=0):
     if request.method == 'GET':
         context = {"prod_num" : prod_num}
@@ -179,6 +182,7 @@ def gift_card_view(request, prod_num=0):
         card_file.close()
         return render(request, f"gift.html", context)
 
+@csrf_protect
 def use_card_view(request):
     context = {'card_found':None}
     if request.method == 'GET':
